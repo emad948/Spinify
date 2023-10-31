cbbe1c68eccba3306b9d56220067a9b98a130e3c
ealmsouti <emad.almsouti@syracom.de>
2023-10-31 23:39:44 +0100

    improve eslint rules

--- App/App.tsx
+++ App/App.tsx
@@ -1,14 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-      <View className="flex-1 items-center justify-center bg-amber-200 ">
-
-
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+    <View className="flex-1 items-center justify-center bg-amber-200 ">
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
-
